ca79a65abe8d1c4ef78f5c7c85b5096e999c931f
Erik Cheever <echeeve1@swarthmore.edu>
2015-06-18 13:27:15 -0400

    3014
    Added code to use any serial port (not just COM1).

--- TektronixTDS3014GUI/MatlabTDS3014.m
+++ TektronixTDS3014GUI/MatlabTDS3014.m
@@ -5,7 +5,7 @@ function varargout = MatlabTDS3014(varargin)
 %
 %      MATLABTDS3014, by itself, creates a new MATLABTDS3014 or raises the existing
 %      singleton*.
-% 
+%
 %      H = MATLABTDS3014 returns the handle to a new MATLABTDS3014 or the handle to
 %      the existing singleton*.
 %
@@ -28,15 +28,16 @@ function varargout = MatlabTDS3014(varargin)
 % Last Modified by GUIDE v2.5 10-Aug-2009 13:10:15
 
 % Begin initialization code - DO NOT EDIT
+%#ok<*DEFNU>
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
-                   'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @MatlabTDS3014_OpeningFcn, ...
-                   'gui_OutputFcn',  @MatlabTDS3014_OutputFcn, ...
-                   'gui_LayoutFcn',  [], ...
-                   'gui_Callback',   []);
+    'gui_Singleton',  gui_Singleton, ...
+    'gui_OpeningFcn', @MatlabTDS3014_OpeningFcn, ...
+    'gui_OutputFcn',  @MatlabTDS3014_OutputFcn, ...
+    'gui_LayoutFcn',  [], ...
+    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
-   gui_State.gui_Callback = str2func(varargin{1});
+    gui_State.gui_Callback = str2func(varargin{1});
 end
 
 if nargout
@@ -48,7 +49,7 @@ end
 
 
 % --- Executes just before MatlabTDS3014 is made visible.
-function MatlabTDS3014_OpeningFcn(hObject, eventdata, handles, varargin)
+function MatlabTDS3014_OpeningFcn(hObject, ~, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -87,7 +88,7 @@ showAxes(handles);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = MatlabTDS3014_OutputFcn(hObject, eventdata, handles)
+function varargout = MatlabTDS3014_OutputFcn(~, ~, handles)
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -98,12 +99,12 @@ varargout{1} = handles.output;
 
 
 % --- Executes on button press in getdataPB.
-function getdataPB_Callback(hObject, eventdata, handles)
+function getdataPB_Callback(~, ~, handles) 
 hwaitbar=waitbar(0,'Download Progress');
 s=handles.serial;
 set(s,'Timeout',10); %defines 10s as time for timeout
 
-numCollect=str2num(query(s,'HOR:RECORD?'))
+numCollect=str2double(query(s,'HOR:RECORD?'));
 
 if numCollect==10000,
     numAdd=8;
@@ -113,7 +114,7 @@ end
 handles.numCollect=numCollect;
 
 %Get time per div data, stores in handle, displays
-handles.tdiv=str2num(query(s,'HOR:MAI:SCA?'));
+handles.tdiv=str2double(query(s,'HOR:MAI:SCA?'));
 divisionTime=handles.tdiv;
 if divisionTime<1E-3,
     tMul=1E6;  tUnits='(uS)';
@@ -122,7 +123,7 @@ elseif divisionTime<1,
 else
     tMul=1; tUnits='(S)';
 end
-        
+
 
 set(handles.tdivText,'String',num2str(handles.tdiv*tMul));
 set(handles.tDiv_PANEL,'Title',['Time/Div ' tUnits]);
@@ -136,11 +137,11 @@ for ch=1:4,
     chS=num2str(ch);      %String representation of channel for cell data
     if handles.channels.chanSelect(ch*2-1)=='1',  % If channel is active.
         handles.channels.state(ch)=1; %stores that state
-        handles.channels.scale(ch)=str2num(query(s,['CH' chS ':SCA?']));
+        handles.channels.scale(ch)=str2double(query(s,['CH' chS ':SCA?']));
         fprintf(s,['DAT:SOU CH' chS]);
         
         fprintf(s,'CURV?');
-        [s1, cnt]=fread(s, 2*numCollect+numAdd); %fread is specifically for binary data
+        s1=fread(s, 2*numCollect+numAdd); %fread is specifically for binary data
         %pulls and stores curve data
         handles.channels.wfmpre{ch}=query(s,'WFMPre?');
         s1=s1(numAdd:(end-1)); %Tuncates data from preamble and postamble
@@ -155,16 +156,20 @@ for ch=1:4,
         set(handles.vDivText(ch),'Visible','on');
         set(handles.vDivText(ch),'String',num2str(handles.channels.scale(ch)));
         
-        %Checks and reports the coupling status to the display 
+        %Checks and reports the coupling status to the display
         chanelcoupling=query(s,['CH' chS ':COUP?']);
         set(handles.coupling(ch),'Visible','on');
         set(handles.coupling(ch),'string', chanelcoupling);
-         
         
-        handles.offset.vshift{ch}=str2num(query(s, ['CH' chS ':POS?'])); %Finds the vertical shift 
+        
+        handles.offset.vshift{ch}=...
+            str2double(query(s, ['CH' chS ':POS?'])); %Find vertical shift
         %set(handles.MatlabTDS3014fig, 'CurrentAxes', handles.scopeAx); %this brings the axes to the front!!
-        handles.channels.bytevoltconvert{ch}=str2num(query(s,'WFMP:YMU?'));
-        handles.channels.vshiftvolts{ch}=handles.channels.scale(ch)*str2num(query(s, ['CH' chS ':POS?']));
+        handles.channels.bytevoltconvert{ch}=...
+            str2double(query(s,'WFMP:YMU?'));
+        handles.channels.vshiftvolts{ch}=...
+            handles.channels.scale(ch)*...
+            str2double(query(s, ['CH' chS ':POS?']));
         
         
     else
@@ -173,11 +178,12 @@ for ch=1:4,
         handles.channels.scale(ch)=-inf;
         handles.channels.wfmpre{ch}=-inf;
         handles.channels.wfmdat{ch}=-inf;
- 
+        
         set(handles.chanText(ch),'FontWeight','light');
         set(handles.chanText(ch),'Enable','off');
         set(handles.vDivText(ch),'Visible','off');
-        set(handles.vDivText(ch),'String',num2str(handles.channels.scale(ch)));
+        set(handles.vDivText(ch),'String',...
+            num2str(handles.channels.scale(ch)));
     end
     waitbar(0.2+ch/5,hwaitbar)
 end
@@ -187,12 +193,12 @@ close(hwaitbar);
 showAxes(handles); %clears and resets axes for new data
 for ch=1:4
     if handles.channels.chanSelect(ch*2-1)=='1',  % If channel is active.
-        axes(handles.scopeAx);
+        axes(handles.scopeAx); %#ok<LAXES>
         
         %plots ground for each chanel
         plot(0,.25*handles.offset.vshift{ch},'>', 'Color', handles.plotcolor{ch}, ...
-             'MarkerSize',10, 'MarkerFaceColor', handles.plotcolor{ch});
-
+            'MarkerSize',10, 'MarkerFaceColor', handles.plotcolor{ch});
+        
         %Pulls and converts the y-position data from pixels to normed
         %voltages (on a -1 to 1 scale for all channels).
         voltsperdivision=handles.channels.scale(ch);
@@ -205,11 +211,11 @@ end
 
 %Showing the Time Shift
 handles.timeshift.state=query(s, 'HOR:DEL:STATE?'); %whether manual or delay setting is on
-if str2num(handles.timeshift.state)*1==0 %if delay setting is off
-    handles.timeshift.percentmove=str2num(query(s,'HOR:TRIG:POS?')); %done in percents, and shifted
+if str2double(handles.timeshift.state)*1==0 %if delay setting is off
+    handles.timeshift.percentmove=str2double(query(s,'HOR:TRIG:POS?')); %done in percents, and shifted
     plot(numCollect*0.01*handles.timeshift.percentmove,1, 'rv', 'MarkerSize',8, 'MarkerFaceColor', 'r');
 else %delay setting is on
-    handles.timeshift.timemove=str2num(query(s,'Hor:Del:Tim?')); %done in terms of time
+    handles.timeshift.timemove=str2double(query(s,'Hor:Del:Tim?')); %done in terms of time
     plot((numCollect/2)-(handles.timeshift.timemove*numCollect/(10*divisionTime)), 1,'rv', 'MarkerSize',8, 'MarkerFaceColor', 'r');
 end
 
@@ -224,34 +230,36 @@ guidata(handles.MatlabTDS3014fig, handles);
 
 
 % --- Executes on button press in savedataPB.
-function savedataPB_Callback(hObject, eventdata, handles)
+function savedataPB_Callback(~, ~, handles)
 %Get time per div data.
 divisionTime=handles.tdiv; %pulls parameters from handles
-numCollect=handles.numCollect; 
+numCollect=handles.numCollect;
 xstep=divisionTime*10/numCollect;
-range=[-numCollect/2:1:numCollect/2-1]'; %To map the time onto the indicies of the data
+range=(-numCollect/2:1:numCollect/2-1)'; %To map the time onto the indicies of the data
 xintermediate=xstep.*range; %creates a time vector
 
 %Time Shift
-if str2num(handles.timeshift.state)*1==0 %whether or not delay is used
+if str2double(handles.timeshift.state)*1==0 %whether or not delay is used
     actualshift=5*divisionTime-divisionTime*0.1*handles.timeshift.percentmove; %done with percentages
-else 
+else
     actualshift=(handles.timeshift.timemove); %simple time shift
 end
 x=xintermediate+actualshift; %adding the shift to the time vector
 
+data = cell(4,1);  % Preallocate.
 for ch=1:4,
-    if handles.channels.chanSelect(ch*2-1)=='1',  % If channel is active.  
-        y=(handles.offset.withoutoffset{ch}-handles.channels.vshiftvolts{ch}); %defines the output vector and saves
+    if handles.channels.chanSelect(ch*2-1)=='1',  % If channel is active.
+        y=(handles.offset.withoutoffset{ch}-...
+            handles.channels.vshiftvolts{ch}); %defines output vector and saves
         data{ch}=y;
-    else 
+    else
         data{ch}=[];
     end
 end
 
 %User prompt for comment
 comm=inputdlg('Enter Comment for Data File','Comment');
-fileComment=[comm{1} '; Date:' datestr(clock)]; 
+fileComment=[comm{1} '; Date:' datestr(clock)]; %#ok<NASGU>
 
 %User prompt to ssave
 [fname,pname]=uiputfile('*.mat', 'Save Scope Data');
@@ -260,25 +268,25 @@ if fname==0,
     warndlg('Problem with Filename.', 'Filename Problem.','modal');
     return;
 end
-t=x;
-save([pname fname], 't','data', 'fileComment');
+t=x; %#ok<NASGU>
+save([pname fname], 't', 'data', 'fileComment');
 
 
 % --- Executes on button press in printPB.
-function printPB_Callback(hObject, eventdata, handles)
+function printPB_Callback(~, ~, handles)
 set(handles.MatlabTDS3014fig,'PaperPositionMode','auto')
 set(handles.MatlabTDS3014fig, 'InvertHardcopy','off')
 print -v
 
 
 % --- Executes on button press in exitPB.
-function exitPB_Callback(hObject, eventdata, handles)
+function exitPB_Callback(~, ~, handles)
 close(handles.MatlabTDS3014fig);
 
 
 % --- Executes on button press in helpbutton.
-function helpbutton_Callback(hObject, eventdata, handles)
-web('http://www.swarthmore.edu//NatSci/echeeve1/Ref/TDS4013/MatLabTDS4013.html','-browser')
+function helpbutton_Callback(~, ~, ~)
+web('http://www.swarthmore.edu/NatSci/echeeve1/Ref/TDS3014/MatlabTDS3014.html','-browser')
 
 function showAxes(handles)
 %this function sets and clears the axes on the display, with the center
@@ -288,61 +296,89 @@ numPts=handles.numCollect;
 set(handles.scopeAx, 'box','on',...
     'XLim', [0,numPts],...
     'Xgrid','on',...
-    'XTick', [0:numPts/10:numPts],...
+    'XTick', 0:numPts/10:numPts,...
     'YLim', [-1,1],...
     'Ygrid', 'on',...
-    'YTick', [-1:1/4:1],...
+    'YTick', -1:1/4:1,...
     'XMinorTick', 'on',...
     'YMinorTick', 'on',...
     'XTickLabel',{''},...
-    'YTickLabel',{''}); 
+    'YTickLabel',{''});
 hold on
-plot([0 numPts], [0 0],'Color','k','Linewidth', 1.5); 
+plot([0 numPts], [0 0],'Color','k','Linewidth', 1.5);
 plot([numPts/2 numPts/2], [-1 1],'Color','k','Linewidth', 1.5);
 
 function initGUI(handles)
-openPorts=instrfind('Port','COM1');
+
+openPorts=instrfind;
 if ~isempty(openPorts),
     fclose(openPorts); %Close all open serial connections
     delete(openPorts);
 end
 
-%Defines the serial connection needed.
+mySer = instrhwinfo('serial');
+numSer = length(mySer.AvailableSerialPorts);
+waitbarInc = 1/(1+numSer);
+waitbarVal = waitbarInc;
+hWait = waitbar(waitbarVal,'Trying to establish contact');
 
-s=serial('COM1',...
-    'baud',38400,...
-    'FlowControl','hardware',...
-    'InputBufferSize',50000,...
-    'Timeout',1,...
-    'ReadAsyncMode','continuous');
-fopen(s);
+%Open each serial port in succession;
+success = 0;
+i = 0;
+% Convert unsuccessful read to error temporarily (for try/catch in loop)
+   w2e=warning('error', 'instrument:query:unsuccessfulRead');
+while ~success,
+    i=i+1;
+    waitbarVal = waitbarVal + waitbarInc;
+    hWait = waitbar(waitbarVal, hWait, ...
+        ['Trying ' mySer.AvailableSerialPorts{i}]);
+    s=serial(mySer.AvailableSerialPorts{i},...
+        'baud',38400,...
+        'FlowControl','hardware',...
+        'InputBufferSize',50000,...
+        'Timeout',1,...
+        'ReadAsyncMode','continuous',...
+        'Terminator','LF'); %#ok<TNMLP>
+    fopen(s);
+    
 
-% Try to open, program fails at query if connection isn't made.
-w={'Warning...',...
-    'Unable to establish communications with oscilloscope.',' ',...
-    'Check that:'...
-    '1) Baud rates are equal (On scope hit ''Utility'' button,',...
-    '   then ''System'' to get to I/O menu,',...
-    '   then set parameters to default,'...
-    '   then change baud rate to 38400), and'...
-    '2) Serial port cable is connected from scope to computer, and'...
-    '3) The oscilloscope is on.'};
-warnHandle=warndlg(w,'Serial Comm. Error','on-modal');
-idString=query(s,'ID?');
-close(warnHandle);
+    try
+        idString = query(s,'ID?');
+    catch
+        idString = [];  % If timeout, set idString to null
+    end
 
-if isempty(findstr(idString,'3014B')),
-    beep;                       %Warning for if communications did not work.
-    warndlg(w,'Serial Comm. Error','modal')
-    close(handles.MatlabTDS3014fig);
-    return
+    if ~isempty(strfind(idString,'3014B')),
+        success = 1;
+    else
+        fclose(s);
+        delete(s);
+    end
 end
+close(hWait);
+    warning(w2e);  %Convert back to warning.
+    
+if ~success,
+    % Try to open, program fails at query if connection isn't made.
+    w={'Warning...',...
+        'Unable to establish communications with oscilloscope.',' ',...
+        'Check that:'...
+        '1) Baud rates are equal (On scope hit ''Utility'' button,',...
+        '   then ''System'' to get to I/O menu,',...
+        '   then set parameters to default,'...
+        '   then change baud rate to 38400), and'...
+        '2) Serial port cable is connected from scope to computer, and'...
+        '3) The oscilloscope is on.'};
+    warnHandle=warndlg(w,'Serial Comm. Error','on-modal');
+    close(warnHandle);
+end
+
 
 fprintf(s,'DAT:ENC RPB');       %Takes the data in 16 bit binary
 fprintf(s,'DAT:WID 2');         %2 bytes of data
-fprintf(s,['HOR:RECORD 500']);  %500 points
+fprintf(s,'HOR:RECORD 500');  %500 points
 fprintf(s,'DAT:STAR 1');        %Start with first point
-fprintf(s,['DAT:STOP 500']);    %End with 500th point
+fprintf(s,'DAT:STOP 500');    %End with 500th point
 
 handles.serial=s;
 %setting the text off pending data reception
@@ -367,17 +403,17 @@ guidata(handles.MatlabTDS3014fig, handles);
 
 
 % --- Executes on button press in numPts500_RB.
-function numPts500_RB_Callback(hObject, eventdata, handles)
+function numPts500_RB_Callback(~, ~, handles)
 s=handles.serial;
 set(s,'Timeout',1); %defines 1s as time for timeout
-fprintf(s,['HOR:RECORD 500']); 
-fprintf(s,['DAT:STOP 500']);
+fprintf(s,'HOR:RECORD 500');
+fprintf(s,'DAT:STOP 500');
 
 
 % --- Executes on button press in numPts10000_RB.
-function numPts10000_RB_Callback(hObject, eventdata, handles)
+function numPts10000_RB_Callback(~, ~, handles)
 s=handles.serial;
 set(s,'Timeout',1); %defines 1s as time for timeout
-fprintf(s,['HOR:RECORD 10000']); 
-fprintf(s,['DAT:STOP 10000']);
+fprintf(s,'HOR:RECORD 10000');
+fprintf(s,'DAT:STOP 10000');
 
